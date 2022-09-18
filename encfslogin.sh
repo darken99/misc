67fbd595981cdc600f896e5acce51330885e85b8
Oleksandr Moskalenko <omoskale@amazon.com>
2022-09-18 13:40:14 +0200

    Update encfs script

--- encfslogin.sh
+++ encfslogin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ENCFS="/usr/local/bin/encfs" 
-ENCDIR="$HOME/Dropbox/Boxcryptor Classic.bc"
+ENCDIR="$HOME/Library/CloudStorage/Dropbox/Boxcryptor Classic.bc"
 DECDIR="$HOME/BoxCryptor"
 
 security find-generic-password -ga encfs 2>&1 >/dev/null | cut -d'"' -f2 | "$ENCFS" -S "$ENCDIR" "$DECDIR"
