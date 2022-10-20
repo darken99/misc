284d2772db554ee6f11c0655fa05d5fa8f857b1a
Oleksandr Moskalenko <omoskale@amazon.com>
2022-10-20 20:39:30 +0200

    Import

--- backup_dshure.sh
+++ backup_dshure.sh
@@ -1,8 +1,8 @@
-#!/bin/env sh
+#!/usr/bin/env sh
 
 if [ -d '/Volumes/DATASHUR' ]; then
     a2scp desktop:\*.kdbx ~/BoxCryptor/security/keepass/
-    zip -r /Volumes/DATASHUR/keepass-$(date +"%Y%m%d").zip ~/Dropbox/keepass_main_db.*
-    zip -r /Volumes/DATASHUR/security-$(date +"%Y%m%d").zip ~/BoxCryptor/security/*
-    zip -r /Volumes/DATASHUR/archive-$(date +"%Y%m%d").zip ~/BoxCryptor/_Archive/*
+    zip -qr /Volumes/DATASHUR/keepass-$(date +"%Y%m%d").zip ~/Dropbox/keepass_main_db.*
+    zip -qr /Volumes/DATASHUR/security-$(date +"%Y%m%d").zip ~/BoxCryptor/security/*
+    zip -qr /Volumes/DATASHUR/archive-$(date +"%Y%m%d").zip ~/BoxCryptor/_Archive/*
 fi
